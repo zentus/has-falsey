8849f8778160ce40f53a5ef57d76582753709c08
zentus <zentus@cimantra.com>
2019-04-27 03:58:30 +0200

    Refactor

--- source/index.js
+++ source/index.js
@@ -1,8 +1,7 @@
 const hasFalsey = input => {
-	if (Array.isArray(input)) {
-		return Boolean(input.find(item => !item))
-	}
-	throw new Error(`has-falsey expected an array, got "${input}". typeof: ${typeof input}`)
+  if (Array.isArray(input)) return Boolean(input.find(item => !item))
+  
+  throw new Error(`has-falsey expected an array, got "${input}". typeof: ${typeof input}`)
 }
 
 module.exports = hasFalsey
